516c35746916a19c5be9752d5397d08948026dce
Brian Gibson <bggibson@hotmail.com>
2017-01-07 15:38:19 +0100

    Refactored rentalLines and rentalLine methods

--- src/Statement.java
+++ src/Statement.java
@@ -36,38 +36,47 @@ class Statement {
 
     private String rentalLines() {
         String rentalLines = "";
-        for (Rental rental : rentals) {
-            double thisAmount = 0;
-
-            // determines the amount for each line
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.getDaysRented() > 2)
-                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                    && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            rentalLines += "\t" + rental.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-
-        }
+        for (Rental rental : rentals)
+            rentalLines += rentalLine(rental);
         return rentalLines;
+    }
+
+    private String rentalLine(Rental rental) {
+        double rentalAmount = determineRentalAmount(rental);
+        frequentRenterPoints += determineFrequentRenterPoints(rental);
+        totalAmount += rentalAmount;
+        return formatRentalLine(rental, rentalAmount);
+    }
+
+    private double determineRentalAmount(Rental rental) {
+        double rentalAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    rentalAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    rentalAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
+
+    private int determineFrequentRenterPoints(Rental rental) {
+        boolean bonusIsEarned = rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
+        if (bonusIsEarned)
+            return 2;
+        return 1;
+    }
+
+    private String formatRentalLine(Rental rental, double rentalAmount) {
+        return String.format("\t%s\t%.1f\n", rental.getMovie().getTitle(), rentalAmount);
     }
 
     private String footer() {
